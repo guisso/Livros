57d834eb8ca974b66125c2fcb76951a892729737
guisso <lguisso@gmail.com>
2022-05-20 16:20:25 -0300

    Organização do código-fonte

--- src/main/java/io/github/guisso/livros/gui/mycomponent/CheckboxListCellRenderer.java
+++ src/main/java/io/github/guisso/livros/gui/mycomponent/CheckboxListCellRenderer.java
@@ -14,17 +14,19 @@ import javax.swing.ListCellRenderer;
 /**
  * A JList component within JCheckBox items.
  *
- * Source: http://link-intersystems.com/blog/2014/10/19/custom-swing-component-renderers/
- * Source: https://github.com/link-intersystems/blog/tree/master/custom-ListCellRenderer
- * 
+ * Source:
+ * http://link-intersystems.com/blog/2014/10/19/custom-swing-component-renderers/
+ * Source:
+ * https://github.com/link-intersystems/blog/tree/master/custom-ListCellRenderer
+ *
  * <pre>JList<Autor> list = new JList<Autor>();
  * list.setCellRenderer(new CheckboxListCellRenderer<Autor>());</pre>
  *
  * @author Luis Guisso <luis dot guisso at ifnmg dot edu dot br>
  * @version 0.0.1, 22/08/2021
  */
-public class CheckboxListCellRenderer<E> 
-        extends JCheckBox 
+public class CheckboxListCellRenderer<E>
+        extends JCheckBox
         implements ListCellRenderer<E> {
 
     private static final long serialVersionUID = 3734536442230283966L;
@@ -46,7 +48,5 @@ public class CheckboxListCellRenderer<E>
 
         return this;
     }
-    
-    
 
 }
